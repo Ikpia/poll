3040a4a5a22ec997c89c71513a1ed79c4905e557
Ikpia <echukwuka97@gmail.com>
2024-02-04 00:00:17 +0100

    added polls and ballot fields

--- src/state.rs
+++ src/state.rs
@@ -2,11 +2,25 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cw_storage_plus::{Item, Map};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct Config {
     pub admin: Addr,
 }
 
+#[derive(Serialize, Deserialize, Clone, PartialEq, Debug, JsonSchema)]
+pub struct Poll {
+    pub admin: Addr,
+    pub question: String,
+    pub options: Vec<(String, u64)>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, JsonSchema)]
+pub struct Ballot {
+    pub option: String,
+}
+
 pub const CONFIG: Item<Config> = Item::new("config");
+pub const POLL: Map<String, Poll> = Map::new("polls");
+pub const BALLOT: Map<(Addr, String), Ballot> = Map::new("ballots");
